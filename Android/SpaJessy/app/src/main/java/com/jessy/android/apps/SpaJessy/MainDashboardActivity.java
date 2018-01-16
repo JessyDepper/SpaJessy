bb53f5cb608b7de199ef9d3510cf14be4b4848d6
JessyDepper <minhnk.1607@gmail.com>
2018-01-17 05:24:28 +0700

    Add new project

---
+++ Android/SpaJessy/app/src/main/java/com/jessy/android/apps/SpaJessy/MainDashboardActivity.java
@@ -0,0 +1,53 @@
+package com.jessy.android.apps.SpaJessy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.jessy.android.apps.SpaJessy.helper.BottomNavigationViewHelper;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_new_feed:
+                    mTextMessage.setText(R.string.title_new_feed);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_messenger:
+                    mTextMessage.setText(R.string.title_messenger);
+                    return true;
+                case R.id.navigation_manager:
+                    mTextMessage.setText(R.string.title_manager);
+                    return true;
+                case R.id.navigation_more:
+                    mTextMessage.setText(R.string.title_more);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_dashboard);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.removeShiftMode(navigation);
+    }
+
+}
