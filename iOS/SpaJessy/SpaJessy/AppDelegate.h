992952c3e519de8ef1a9c6e2dfdc0cfcf3431eeb
JessyDepper <minhnk.1607@gmail.com>
2018-01-17 05:28:53 +0700

    Add new project

---
+++ iOS/SpaJessy/SpaJessy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpaJessy
+//
+//  Created by Jessy on 1/17/18.
+//  Copyright © 2018 JD Join-stock Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/SpaJessy/SpaJessy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpaJessy
+//
+//  Created by Jessy on 1/17/18.
+//  Copyright © 2018 JD Join-stock Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
